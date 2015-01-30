b5873a82559d386dbfa256cf9b58e34f00f76d0f
Romain Gay <rgay@ocs.online.net>
2015-01-30 01:29:00 +0100

    main function work

--- jswheel.js
+++ jswheel.js
@@ -1,9 +1,34 @@
-'use strict';
-(function($){
+// 'use strict';
 
-    $.jswheel = {};
+function jswheel(wheelData, pointList, options) {
+    this.wheelData = wheelData;
+    this.pointList = pointList;
+    this.options = options;
+    this.index = 0;
+    this.wheelIndex = 0;
+    this.elems = [];
+    this.pLen = this.pointList.length;
+    this.ready = true;
 
-    function pixelScaler(val, y) {
+    if (this.pLen < 2) {
+        throw "jswheel requires at least two points";
+    }
+
+    // if we have less wheel elements than points, multiply wheel elements
+    if (this.wheelData.length < this.pLen) {
+        var toMult = Math.ceil(this.pLen / this.wheelData.length);
+        var newWheelData = this.wheelData;
+        for (var i = 1; i < toMult; i++) {
+            newWheelData = newWheelData.concat(this.wheelData);
+        }
+        this.wheelData = newWheelData;
+    }
+
+    // assures that selection point is the first entry of this.wheelData at start
+    var tmp = this.wheelData.splice(this.wheelData.length - this.options.selectPosition);
+    this.wheelData = tmp.concat(this.wheelData);
+
+    this.pixelScaler = function(val, y) {
         var ref_x = 1024;
         var ref_y = 768;
 
@@ -15,122 +40,97 @@
         } else {
             return (val * win_x / ref_x);
         }
-    }
-
-    $.jswheel = function(wheelData, pointList, options) {
-        var res = {};
-        var index = 0;
-        var wheelIndex = 0;
-        var elems = [];
-        var pLen = pointList.length;
-        var ready = true;
-        var tmp;
-
-        if (pLen < 2) {
-            throw "jswheel requires at least two points";
-        }
-
-        // if we have less wheel elements than points, multiply wheel elements
-        if (wheelData.length < pLen) {
-            var toMult = Math.ceil(pLen / wheelData.length);
-            var newWheelData = wheelData;
-            for (var i = 1; i < toMult; i++) {
-                newWheelData = newWheelData.concat(wheelData);
-            }
-            wheelData = newWheelData;
-        }
-
-        // assures that selection point is the first entry of wheelData at start
-        tmp = wheelData.splice(wheelData.length - options.selectPosition);
-        wheelData = tmp.concat(wheelData);
-
-        // reset / set positions of all elements
-        res.update = function () {
-            elems = [];
-            $(options.container).empty();
-
-            for (var e = 0; e < pLen; e++) {
-                var cur = (e + index) % pLen;
-                var wheelCur = (cur + wheelIndex) % wheelData.length;
-
-                var elemHTML = [
-                    '<img class="ws-elem"',
-                      'src="'+wheelData[wheelCur].file+'"',
-                      'style="'+options.style+'"',
-                      'title="'+wheelData[wheelCur].name+'">',
-                    '</img>'
-                ].join(' ');
-
-                var elemTransform = [
-                    'translate('+pixelScaler(pointList[cur][0], 0)+'px,'+pixelScaler(pointList[cur][1], 1)+'px)',
-                    'rotate('+pointList[cur][2]+'deg)',
-                    'scale('+pointList[cur][3]+')',
-                ].join(' ');
-
-                var newelem = $(elemHTML).css({
-                    'transform': elemTransform,
-                    'z-index': pointList[cur][4],
-                }).appendTo($(options.container));
-
-                elems.push(newelem);
-            }
-        };
-
-        res.move = function (to) {
-            var toChange, curWheelIndex;
-
-            // avoids animation queuing
-            if (ready) {
-                ready = false;
-                setTimeout(function() {ready = true;}, options.transitionTime);
-            } else {
-                return;
-            }
-
-            if (to == 'prev') {
-                index = (index + 1) % pLen;
-                wheelIndex = (wheelIndex + wheelData.length - 1) % wheelData.length;
-                toChange = (pLen - index) % pLen;
-                curWheelIndex = wheelIndex;
-
-            } else {
-                index = (index + pLen - 1) % pLen;
-                wheelIndex = (wheelIndex + 1) % wheelData.length;
-                toChange = pLen - 1 - index;
-                curWheelIndex = (wheelIndex+pLen-1) % wheelData.length;
-            }
-
-            // change element image and name
-            elems[toChange].attr('src', wheelData[curWheelIndex].file)
-                           .attr('name', wheelData[curWheelIndex].name);
-
-            // update elements positions, rotation, etc..
-            for (var e = 0; e < pLen; e++) {
-                var cur = (e + index) % pLen;
-                var elemTransform = [
-                    'translate('+pixelScaler(pointList[cur][0], 0)+'px,'+pixelScaler(pointList[cur][1], 1)+'px)',
-                    'rotate('+pointList[cur][2]+'deg)',
-                    'scale('+pointList[cur][3]+')',
-                ].join(' ');
-                elems[e].css({"z-index":pointList[cur][4]});
-                elems[e].animate({transform: elemTransform}, options.transitionTime);
-            }
-        };
-
-        res.prev = function () {
-            res.move('prev');
-        };
-
-        res.next = function () {
-            res.move('next');
-        };
-
-        res.select = function () {
-            return (wheelData[(wheelIndex + options.selectPosition) % wheelData.length]);
-        };
-
-        res.update();
-        return res;
     };
 
-})(jQuery);
+    // reset / set positions of all elements
+    this.update = function () {
+        this.elems = [];
+        $(this.options.container).empty();
+
+        for (var e = 0; e < this.pLen; e++) {
+            var cur = (e + this.index) % this.pLen;
+            var wheelCur = (cur + this.wheelIndex) % this.wheelData.length;
+
+            var elemHTML = [
+                '<img class="ws-elem"',
+                  'src="'+this.wheelData[wheelCur].file+'"',
+                  'style="'+this.options.style+'"',
+                  'title="'+this.wheelData[wheelCur].name+'">',
+                '</img>'
+            ].join(' ');
+
+            var elemTransform = [
+                'translate('+this.pixelScaler(this.pointList[cur][0], 0)+'px,'+
+                             this.pixelScaler(this.pointList[cur][1], 1)+'px)',
+                'rotate('+this.pointList[cur][2]+'deg)',
+                'scale('+this.pointList[cur][3]+')',
+            ].join(' ');
+
+            var newelem = $(elemHTML).css({
+                'transform': elemTransform,
+                'z-index': this.pointList[cur][4],
+            }).appendTo($(this.options.container));
+
+            this.elems.push(newelem);
+        }
+    };
+
+    this.move = function (to) {
+        var toChange, curWheelIndex;
+
+        // avoids animation queuing
+        if (this.ready) {
+            parent = this;
+            this.ready = false;
+            setTimeout(function() {parent.ready = true;}, this.options.transitionTime);
+        } else {
+            return;
+        }
+
+        if (to == 'prev') {
+            this.index = (this.index + 1) % this.pLen;
+            this.wheelIndex = (this.wheelIndex + this.wheelData.length - 1) %
+                              this.wheelData.length;
+            toChange = (this.pLen - this.index) % this.pLen;
+            curWheelIndex = this.wheelIndex;
+
+        } else {
+            this.index = (this.index + this.pLen - 1) % this.pLen;
+            this.wheelIndex = (this.wheelIndex + 1) % this.wheelData.length;
+            toChange = this.pLen - 1 - this.index;
+            curWheelIndex = (this.wheelIndex + this.pLen - 1) % this.wheelData.length;
+        }
+
+        // change element image and name
+        this.elems[toChange].attr('src', this.wheelData[curWheelIndex].file)
+                            .attr('name', this.wheelData[curWheelIndex].name);
+
+        // update elements positions, rotation, etc..
+        for (var e = 0; e < this.pLen; e++) {
+            var cur = (e + this.index) % this.pLen;
+            var elemTransform = [
+                'translate('+this.pixelScaler(pointList[cur][0], 0)+'px,'+
+                             this.pixelScaler(pointList[cur][1], 1)+'px)',
+                'rotate('+pointList[cur][2]+'deg)',
+                'scale('+pointList[cur][3]+')',
+            ].join(' ');
+            this.elems[e].css({"z-index":pointList[cur][4]});
+            this.elems[e].animate({transform: elemTransform}, this.options.transitionTime);
+        }
+    };
+
+    this.prev = function () {
+        this.move('prev');
+    };
+
+    this.next = function () {
+        this.move('next');
+    };
+
+    this.select = function () {
+        return (this.wheelData[(this.wheelIndex + this.options.selectPosition) %
+               this.wheelData.length]);
+    };
+
+    this.update();
+};
